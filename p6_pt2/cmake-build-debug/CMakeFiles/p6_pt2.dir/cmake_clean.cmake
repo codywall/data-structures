486e3ce72785ea7072f6b72d70e82b7b40c3091c
codywall <cody.wall@gmail.com>
2019-11-30 22:36:06 -0800

    finished stack assignment

---
+++ p6_pt2/cmake-build-debug/CMakeFiles/p6_pt2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p6_pt2.dir/main.cpp.o"
+  "p6_pt2.pdb"
+  "p6_pt2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p6_pt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
